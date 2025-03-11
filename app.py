5e218c633c2ebea08ef8728b252523848b3da379
Omkar31415 <88371971+Omkar31415@users.noreply.github.com>
2025-03-11 17:31:16 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import numpy as np
+import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# Load the tokenizer
+with open('tokenizer.pickle','rb') as handle:
+    tokenizer=pickle.load(handle)
+
+# Function to predict the next word
+def predict_next_word(model, tokenizer, text, max_sequence_len):
+    token_list = tokenizer.texts_to_sequences([text])[0]
+    if len(token_list) >= max_sequence_len:
+        token_list = token_list[-(max_sequence_len-1):]
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+    predicted = model.predict(token_list, verbose=0)
+    predicted_word_index = np.argmax(predicted, axis=1)
+    for word, index in tokenizer.word_index.items():
+        if index == predicted_word_index:
+            return word
+    return None
+
+# Streamlit app
+st.title("Next Word Prediction")
+
+# Model selection
+model_options = {
+    "LSTM": "next_word_lstm.h5",
+    "Bi-LSTM": "next_word_bilstm.h5",
+    "GRU": "next_word_gru.h5",
+    "Bi-GRU": "next_word_bigru.h5"
+}
+selected_model = st.selectbox("Select a model", list(model_options.keys()))
+
+# Load the selected model
+model = load_model(model_options[selected_model])
+max_sequence_len = model.input_shape[1] + 1
+
+input_text = st.text_input("Enter the sequence of words", "To be or not to")
+
+if st.button("Predict Next Word"):
+    next_word = predict_next_word(model, tokenizer, input_text, max_sequence_len)
+    st.write(f"Next word: {next_word}")
